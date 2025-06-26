f73ede83643d6fad5e0bf0648871f9e0957a1b21
JediMester <142922715+JediMester@users.noreply.github.com>
2025-06-26 19:32:32 +0200

    Create multi-monitor.sh

---
+++ multi-monitor.sh
@@ -0,0 +1,4 @@
+#!/run/current-system/sw/bin/bash
+
+xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+       --output eDP-1-1 --mode 1920x1080 --pos 2560x0 --rotate normal
